8bb8809a5d2cfda963cef3bd19d370b12a3c2814
Name <example@gmail.com>
2026-07-27 23:34:59 +0400

    remove tovari

--- products/goods/fen-brash.js
+++ products/goods/fen-brash.js
@@ -2,6 +2,7 @@ PRODUCTS.goods.push({
   name: "Фен-браш плюс насадка",
   description: "Пользовалась пару раз",
   price: 600,
+  sold: true,
   photos: [
     "assets/fen-brash-1.jpg",
     "assets/fen-brash-2.jpg"
--- products/goods/organayzer-setka.js
+++ products/goods/organayzer-setka.js
@@ -1,6 +1,6 @@
 PRODUCTS.goods.push({
   name: "Органайзер сетка",
-  description: "В наличии 3 шт",
+  description: "Осталось 2 шт",
   price: 200,
   photos: [
     "assets/organayzer-setka-1.jpg"
