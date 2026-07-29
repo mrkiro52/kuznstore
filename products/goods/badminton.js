e34985ccfe0d983a35962113e53650e8cd9a0992
Name <example@gmail.com>
2026-07-29 19:09:52 +0400

    some

--- products/goods/badminton.js
+++ products/goods/badminton.js
@@ -2,6 +2,7 @@ PRODUCTS.goods.push({
   name: "Бадминтон, набор",
   description: "Ракетки и воланчики",
   price: 700,
+  sold: true,
   photos: [
     "assets/badminton-1.jpg",
     "assets/badminton-2.jpg"
---
+++ products/goods/zont.js
@@ -0,0 +1,10 @@
+PRODUCTS.goods.push({
+  name: "Зонт",
+  description: "",
+  price: 350,
+  photos: [
+    "assets/zont-1.jpg",
+    "assets/zont-2.jpg",
+    "assets/zont-3.jpg"
+  ]
+});
