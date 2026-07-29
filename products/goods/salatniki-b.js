2ad1f9cd134f01463415033bedbfd491f990a12a
Name <example@gmail.com>
2026-07-30 00:46:21 +0400

    some

--- products/goods/salatniki-b.js
+++ products/goods/salatniki-b.js
@@ -2,6 +2,7 @@ PRODUCTS.goods.push({
   name: "Салатники",
   description: "300₽ за оба вместе",
   price: 300,
+  sold: true,
   photos: [
     "assets/salatniki-b-1.jpg",
     "assets/salatniki-b-2.jpg"
